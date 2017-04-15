3d3c9e62e83b7ffe837fb41267574cfe964f601a
Pavel Oliynyk <pavel.oliynyk@gmail.com>
2017-04-15 11:45:26 +0100

    Final 2

--- src/main/scala/wikipedia/WikipediaRanking.scala
+++ src/main/scala/wikipedia/WikipediaRanking.scala
@@ -20,10 +20,7 @@ object WikipediaRanking {
     "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
     "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
 
-  val conf: SparkConf = new SparkConf()
-    .setAppName("wikipedia")
-    .setMaster("local")
-
+  val conf: SparkConf = new SparkConf().setAppName("wikipedia").setMaster("local")
 
   val sc: SparkContext = new SparkContext(conf)
 
@@ -36,10 +33,8 @@ object WikipediaRanking {
     * Hint2: consider using method `mentionsLanguage` on `WikipediaArticle`
     */
   def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int =
-    rdd.aggregate(0)( (occurences, article) => if (article.text.split(" ").indexOf(lang) >= 0) occurences + 1 else occurences, (a, b) => a + b)
-
-  //rdd.aggregate(0)( { (occurrences, article) =>
-  //   if ( WikipediaArticle(article.title, article.text).mentionsLanguage( lang.toLowerCase() ) ) occurrences + 1 else occurrences }, { (p1 : Int, p2: Int ) => p1 + p2 } )
+    rdd.aggregate(0)( (occurences, article) =>
+      if (article.text.split(" ").indexOf(lang) >= 0) occurences + 1 else occurences, (a, b) => a + b)
 
   /* (1) Use `occurrencesOfLang` to compute the ranking of the languages
    *     (`val langs`) by determining the number of Wikipedia articles that
@@ -49,7 +44,8 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = langs.map(lan => (lan, occurrencesOfLang(lan, rdd)))
+  def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] =
+    langs.map(lan => (lan, occurrencesOfLang(lan, rdd))).sortBy( - _._2 )
 
 
   /* Compute an inverted index of the set of articles, mapping each language
